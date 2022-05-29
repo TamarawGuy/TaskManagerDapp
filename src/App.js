b61ca7053255768bc7e9dd16131f8d841c5da12d
Viktor <viktor.m.rumenov@gmail.com>
2022-05-29 22:53:53 +0200

    Added wallet connection functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import TaskManager from "./artifacts/contracts/TaskManager.sol/TaskManager.json";
+
 const App = () => {
-  return <div>Test</div>;
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [name, setName] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const connect = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      setAccount(accounts[0]);
+      setContract(
+        new ethers.Contract(
+          process.env.REACT_APP_CONTRACT_ADDRESS,
+          TaskManager.abi,
+          signer
+        )
+      );
+    } else {
+      console.log(`Please install metamask`);
+    }
+  };
+
+  useEffect(() => {
+    connect();
+  }, []);
+
+  return <div>{account}</div>;
 };
 
 export default App;
