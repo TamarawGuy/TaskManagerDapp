6403ab173bb19aa9716c89de6f2e2993c6991186
Viktor <viktor.m.rumenov@gmail.com>
2022-05-29 22:14:10 +0200

    Added deploy script for smart contract

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const TaskManager = await ethers.getContractFactory("TaskManager");
+  const taskManager = await TaskManager.deploy();
+  await taskManager.deployed();
+
+  console.log("TaskManager deployed to:", taskManager.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
